1e5549edafecf8650bac5badb9ba3683646f3ec2
Frodon001 <55687537+Frodon001@users.noreply.github.com>
2019-11-11 00:30:28 +0100

    Create Plateau.java

---
+++ unite/Plateau.java
@@ -0,0 +1,19 @@
+
+import Java.util.*;
+
+public class Plateau {
+  
+   private int nbLig;
+   private int nbCol;
+   private string [][] grille;
+   
+public grille(int n, int p){
+   nbLig = n;
+   nbCol = p;
+   grille = new string[nbLig][nbCol];
+   
+   }
+
+
+
+}
