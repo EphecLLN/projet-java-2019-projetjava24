d530502a44b7449fd9541801cb06e7035ab090f5
amaur <amaur@10.99.0.3>
2019-11-15 17:20:12 +0100

    test

--- unite/JoueurTest.java
+++ unite/JoueurTest.java
@@ -10,11 +10,14 @@ import java.awt.Color;
 import org.junit.jupiter.api.Test;
 
 /**
- * @author amaur
- *
+ * @author Crenier Amaury
+ * 
  */
 class JoueurTest {
 
+	/**
+	 * Test method for {@link unite.Joueur#Joueur(String, int)}.
+	 */
 	@Test
 	void testJoueur() {
 		Joueur joueurA = new Joueur("Amaury", 1);
@@ -24,6 +27,10 @@ class JoueurTest {
 		assertEquals(20, joueurA.getReserve());
 	}
 	
+	/**
+	 * Test method for {@link unite.Joueur#getCouleur()}.
+	 */
+	@Test
 	void testGetCouleur() {
 		Joueur joueurA = new Joueur("Amaury", 1);
 		Joueur joueurB = new Joueur("Amaury", 2);
@@ -31,30 +38,43 @@ class JoueurTest {
 		assertEquals(Color.RED, joueurB.getCouleur());
 	}
 	
-	void acheter() {
+	/**
+	 * Test method for {@link unite.Joueur#acheter(unite.Unite)}
+	 */
+	@Test
+	void testAcheter() {
 		Joueur joueurA = new Joueur("Amaury", 1);
 		Pion newSoldat = joueurA.acheter(Unite.BOMBE);
 		assertEquals(21, joueurA.getReserve());
 		assertEquals(Unite.BOMBE, newSoldat);
 	}
 	
+	/**
+	 * Test method for {@link unite.Joueur#payer(unite.Unite)}.
+	 */
+	@Test
 	void testPayer() {
 		Joueur joueurA = new Joueur("Amaury", 1);
 		assertEquals(Unite.BOMBE.getPrice(), joueurA.payer(Unite.BOMBE));
 	}
 	
+	/**
+	 * Test method for {@link unite.Joueur#estPret()}.
+	 */
+	@Test
 	void testEstPret() {
 		Joueur joueurA = new Joueur("Amaury", 1);
 		joueurA.setReserve(0);
 		assertEquals(true, joueurA.estPret());
 	}
 	
+	/**
+	 * Test method for {@link unite.Joueur#retirerReserve()}.
+	 */
+	@Test
 	void testRetirerReserve() {
 		Joueur joueurA = new Joueur("Amaury", 1);
 		joueurA.retirerReserve();
 		assertEquals(19, joueurA.getReserve());
 	}
-	
-	
-	
 }
