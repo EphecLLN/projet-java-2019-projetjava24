a0e7f1a1d767eef2c1b6739fbd092b5286cbe154
amaury <amaur@10.99.0.3>
2019-11-18 17:40:50 +0100

    réglage problème push github

--- model/Pion.java
+++ model/Pion.java
@@ -1,83 +1,117 @@
 package model;
 
+import model.Case;
+import model.Direction;
+import model.Pion;
+import model.Plateau;
+import model.Unite;
 
 public class Pion {
 
 private Unite soldat;
 private Plateau position;
+private Case cases;
 
-
-private int PM;//point de Mouvement
-	/* Les points Mouvement serviront a déterminé la limite de déplacements
-	 * possibles par tour */
-
-	
-
+	/**Constructeur  de la class Pion
+	 * @param nom un pion qui doit être défini dans l'enum Unite pou y récupérer l'ensemble des données
+	 */
 	public Pion(Unite nom) 
 	{
 		soldat =nom;
-		PM = soldat.getDeplace();
+		position.placerUnPion(this, 0, 0);
+		cases = position.getCaseSelectionnee();
+		
 	}
 	
 	
+	/**
+	 * @return
+	 */
 	public String getNom() {return soldat.getName();}
 	
+	/**
+	 * @return
+	 */
+	public Unite getUnite() {return this.soldat;}
 
-	
-	public void combat(Unite ennemi) 
+	/**
+	 * @param ennemi
+	 * @return
+	 */
+	public boolean combat( Pion ennemi) 
 	{
-		if(!soldat.estDetruit(ennemi)) 
+		final Unite allie = this.getUnite();
+		final Unite adversaire = ennemi.getUnite();
+		
+		if(allie.estDetruit(adversaire)) 
 		{
-			if(soldat.getStrength() >= ennemi.getStrength()) 
-			{
-				//si soldat a une force inférieur ou égale à son ennemi il meurt
-			}
-			else 
-			{
-				//si sa force est plus grande il gagne
-			}
+			return false;
 		}
 		else 
 		{
-			//Une unité spéciale est morte 
-		}
-	}	
-	
-	/*public void deplacer(Direction d, int choixDeplacer) 
-	{
-		
-		if(choixDeplacer <= PM)
-		{
-			switch(d) {
-			case HAUT:position.monte(choixDeplacer,getNom(), position.getPosition(this.getNom()));
-			
-				break;
-			case BAS:position.descend(choixDeplacer,getNom(), position.getPosition(this.getNom()));
-			
-				break;
-			case DROITE:position.tourneDroite(choixDeplacer,getNom(), position.getPosition(this.getNom()));
-			
-				break;
-			case GAUCHE:position.tourneGauche(choixDeplacer,getNom(), position.getPosition(this.getNom()));
-			
-				break;
+			if(allie.getStrength() <= adversaire.getStrength()) 
+			{
+				return false;
+			}
+			else 
+			{
+				return true;
 			}
 		}
 		
 	}
 	
-	
-	public void fatigue(int choixDeplacer) 
+	/**
+	 * @param d
+	 * @param nom
+	 */
+	public void deplacer(Direction d, Unite nom)
 	{
-		PM -= choixDeplacer;
 		
-		if(PM == 0) {System.out.println(soldat.getName()+" ne peut plus bouger.");} 
-		else {System.out.println("Il ne vous reste plus que "+ PM +" Point de Mouvement");}
+		final Case caseSelect = this.cardinal(cases,d, nom.getDeplace());
+		
+		if(caseSelect.getPionPresent() != null) 
+		{
+			if(this.combat(caseSelect.getPionPresent())) 
+			{
+				position.deplacerUnPion(cases, caseSelect);
+			}
+			else
+			{
+				position.retirerUnPion(cases);
+			}
+		}
+		else
+		{
+			position.deplacerUnPion(cases, caseSelect);
+		}
+	}	
+	
+	
+	/**
+	 * @param caseOrigine
+	 * @param d
+	 * @param deplace
+	 * @return
+	 */
+	public Case cardinal(Case caseOrigine,Direction d,int deplace) 
+	{
+		switch(d) 
+		{
+		case HAUT: caseOrigine.setCoordY(caseOrigine.getCoordY()+deplace);
+			break;
+		case BAS: caseOrigine.setCoordY(caseOrigine.getCoordY()-deplace);
+			break;
+		case GAUCHE: caseOrigine.setCoordX(caseOrigine.getCoordX()+deplace);
+			break;
+		case DROITE: caseOrigine.setCoordX(caseOrigine.getCoordX()+deplace);
+			break;
+			
+		}
+		
+		return caseOrigine;
 	}
-	
-	public void reset() {this.PM = soldat.getDeplace();}*/
-	
-	
+
 }
 	
 	
