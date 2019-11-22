5d14561a2f5ac5b5b5c45aefa29a062538ac5ee6
HE201695 <HE201695@D155.ephec.be>
2019-11-22 14:38:18 +0100

    finalisation de la méthode combattre()
    suppression de limit dans unite

--- model/Unite.java
+++ model/Unite.java
@@ -2,27 +2,27 @@ package model;
 
 public enum Unite {
 	
-	DRAPEAU("Drapeau",0,0,0,1),
-	BOMBE("Bombe",8,0,100,3),
-	DEMINEUR("Démineur",1,3,160,3),
-	ECLAIREUR("Eclaireur",3,5,100,5),
-	GENERAL("Générale",6,3,700,1),
-	ESPION("Espion",4,3,500,1),
-	CAPORAL("Caporale",5,2,360,5),
-	SABOTEUR("Saboteur",2,2,500,1),
-	TANK("Char d'assaut",7,1,1000,1);
+	DRAPEAU("Drapeau",0,0,0),
+	BOMBE("Bombe",8,0,100),
+	DEMINEUR("Démineur",1,3,160),
+	ECLAIREUR("Eclaireur",3,5,100),
+	GENERAL("Générale",6,3,700),
+	ESPION("Espion",4,3,500),
+	CAPORAL("Caporale",5,2,360),
+	SABOTEUR("Saboteur",2,2,500),
+	TANK("Char d'assaut",7,1,1000);
 	
 	private String nom;
 	
-	private int grade,deplacement,prix,limit;
+	private int grade,deplacement,prix;
 	
 	
-	Unite(String nom, int grade,int deplacement,int prix, int limit) {
+	Unite(String nom, int grade,int deplacement,int prix) {
 		this.nom = nom;
 		this.grade = grade;
 		this.deplacement = deplacement;
 		this.prix = prix;
-		this.limit = limit;
+	
 		
 	}
 	
@@ -36,27 +36,9 @@ public enum Unite {
 
 	public int getPrice() {return prix;}
 	
-	public int getLimit() {return limit;}
+
 	
-	//méthode de différenciation en combat
-	
-	public boolean estDetruit(Unite ennemi) 
-	{
-		switch(this.getName()) 
-		{
-		case "Démineur": return ennemi.getName()=="Bombe";
-		
-		case "Espion": return ennemi.getName()=="Générale";
-		
-		case "saboteur": return ennemi.getName()=="Char d'assaut";
-		
-		default: return false;
-		}
-		
-		
-	}
-	
-	
+
 
 
 }
