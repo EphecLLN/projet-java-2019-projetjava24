d96f27c4b38d021a293aaed833b4badfccfc194c
CrenierAmaury <55687534+CrenierAmaury@users.noreply.github.com>
2019-11-27 18:02:58 +0100

    up (#13)

---
+++ view/StrategoConsole.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package view;
+
+/**
+ * @author amaur
+ *
+ */
+public class StrategoConsole {
+	
+	public StrategoConsole() {
+		
+	}
+	
+	public void testVueConsole(String string) {
+		System.out.println("Création du jeu" + string);
+	}
+}
